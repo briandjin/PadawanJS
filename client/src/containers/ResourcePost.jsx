d6ccf2c4cef327a19f4705eee9f530b3c4872e44
Brian Jin <brianjin@Brians-MacBook-Air-2.local>
2017-04-17 23:11:19 -0700

    Install React-form material

--- client/src/containers/ResourcePost.jsx
+++ client/src/containers/ResourcePost.jsx
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 import {Segment} from 'semantic-ui-react';
 import { postResource } from '../actions/postResourceActions.jsx';
 
 class ResourcePost extends Component {
   render() {
     // native function from redux form
-    console.log('resource post component id', this.props.mentor.id)
     const { fields: {title, category, url, description} } = this.props;
     return (
-      <Segment>
-        <form onSubmit={ this.props.handleSubmit(this.props.postResource, this.props.mentor.id)}>
-          <div>Post Resources</div>
+        <form onSubmit={ this.props.handleSubmit(this.props.postResource)}>
+          <div style={{textAlign: 'center'}}>Post Resources</div>
           <div className="form-group">
             <label>Title</label>
             <input type="text" className="form-control" {...title} />      
           </div>
           <div className="form-group">
             <label>Category</label>
-            <input type="text" className="form-control" {...category} />      
+            <input type="text" className="form-control" {...category} /> 
           </div>
           <div className="form-group">
             <label>URL</label>
@@ -30,20 +28,11 @@ class ResourcePost extends Component {
           </div>
           <button className="btn btn-primary">Submit</button>
         </form>      
-      </Segment>
     )
   }
 }
-function mapStateToProps(state) {
-  return {
-    mentor: state.selectedMentor
-  }
-
-}
-
-// Just like connect except reduxForm takes form fields object as first argument 
 
 export default reduxForm({
   form: 'ResourcePost',
   fields: ['title', 'category', 'URL', 'description']
-}, mapStateToProps, { postResource })(ResourcePost);
+}, null, { postResource })(ResourcePost);
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -5,12 +5,15 @@ import App from './components/App.jsx';
 import HomePage from './components/HomePage.jsx';
 import LoginPage from './containers/LoginPage.jsx';
 import SignUpPage from './containers/SignUpPage.jsx';
-import MentorPage from './components/MentorPage.jsx'
+import MentorPage from './components/MentorPage.jsx';
+import EventPost from './containers/EventPost.jsx';
+import ResourcePost from './containers/ResourcePost.jsx';
 
-export default (<Route component={App}>
-  <Route path='/' component={HomePage} />
-  <Route path="/login" component={LoginPage} />
-  <Route path="/signup" component={SignUpPage} />
-  <Route path="/profile/*" component={MentorPage} />
+export default (
+  <Route component={App}>
+    <Route path='/' component={HomePage} />
+    <Route path="/login" component={LoginPage} />
+    <Route path="/signup" component={SignUpPage} />
+    <Route path="/profile/*" component={MentorPage} /> 
   </Route>
 );
